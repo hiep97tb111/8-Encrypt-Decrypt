72088b03f70e164f01a998c7c33f1e123d5fd0a0
HiepPD <hieppdutck59@gmail.com>
2023-04-12 10:16:44 +0700

    Integrate Encrypt&Decrypt  AES 256 Mode CBC

--- app/src/main/java/com/example/encryptdecrypt/MainAct.kt
+++ app/src/main/java/com/example/encryptdecrypt/MainAct.kt
@@ -1,10 +1,16 @@
 package com.example.encryptdecrypt
 
 import android.os.Bundle
+import android.util.Base64
 import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import java.security.MessageDigest
+import java.security.SecureRandom
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.SecretKeySpec
 
 class MainAct : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,15 +24,92 @@ class MainAct : AppCompatActivity() {
         findViewById<TextView>(R.id.tvHashMD5).setOnClickListener {
             Log.e("Logger", handleHashMD5("Manchester United"))
 
-            if(compareMD5("Manchester United", handleHashMD5("Manchester United"))){
+            if (compareMD5("Manchester United", handleHashMD5("Manchester United"))) {
                 Log.d("Logger", "The input password matches the known hash value")
-            }else{
+            } else {
                 Log.d("Logger", "The input password does not match the known hash value")
             }
         }
 
+        findViewById<TextView>(R.id.tvAES256ModeCBC).setOnClickListener {
+            // Create secretKey or Custom secretKey
+            val keyGenerator: KeyGenerator = KeyGenerator.getInstance("AES")
+            keyGenerator.init(256)
+            val secretKey = keyGenerator.generateKey()
+            val secretKeyToString = Base64.encodeToString(secretKey.encoded,Base64.DEFAULT)
+            Log.e("Logger SecretKey", secretKeyToString)
+
+            handleEncryptAES256ModeCBC("Micheal Carrick", secretKeyToString
+            )
+        }
+
     }
 
+    private fun handleEncryptAES256ModeCBC(plainText: String, encryptionKey: String) {
+        // Create an instance of the cipher with the AES algorithm and CBC mode
+        val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
+
+        // Generate a random initialization vector
+        val ivBytes = ByteArray(16)
+        val random = SecureRandom()
+        random.nextBytes(ivBytes)
+        val iv = IvParameterSpec(ivBytes)
+
+        // Generate a key from the encryptionKey
+        val keyBytes: ByteArray = encryptionKey.toByteArray()
+        val md = MessageDigest.getInstance("SHA-256")
+        val hashedKeyBytes = md.digest(keyBytes)
+        val key = SecretKeySpec(hashedKeyBytes, "AES")
+
+        // Initialize the cipher in encryption mode with the key and initialization vector
+        cipher.init(Cipher.ENCRYPT_MODE, key, iv)
+
+        // Encrypt the plaintext
+        val encryptedBytes = cipher.doFinal(plainText.toByteArray())
+
+        // Combine the initialization vector and encrypted bytes into a single byte array
+        val combinedBytes = ByteArray(ivBytes.size + encryptedBytes.size)
+        System.arraycopy(ivBytes, 0, combinedBytes, 0, ivBytes.size)
+        System.arraycopy(encryptedBytes, 0, combinedBytes, ivBytes.size, encryptedBytes.size)
+
+        // Encode the combined byte array as a Base64 string and return it
+        Log.e("Logger Encode", Base64.encodeToString(combinedBytes, Base64.DEFAULT))
+
+        handleDencryptAES256ModeCBC(Base64.encodeToString(combinedBytes, Base64.DEFAULT), encryptionKey
+        )
+    }
+
+    private fun handleDencryptAES256ModeCBC(encryptedText: String?, encryptionKey: String) {
+        // Decode the Base64-encoded string into a byte array
+        val combinedBytes = Base64.decode(encryptedText, Base64.DEFAULT)
+
+        // Split the byte array into the initialization vector and encrypted bytes
+        val ivBytes = ByteArray(16)
+        System.arraycopy(combinedBytes, 0, ivBytes, 0, ivBytes.size)
+        val encryptedBytes = ByteArray(combinedBytes.size - ivBytes.size)
+        System.arraycopy(combinedBytes, ivBytes.size, encryptedBytes, 0, encryptedBytes.size)
+
+        // Generate a key from the encryptionKey
+        val keyBytes: ByteArray = encryptionKey.toByteArray()
+        val md = MessageDigest.getInstance("SHA-256")
+        val hashedKeyBytes = md.digest(keyBytes)
+        val key = SecretKeySpec(hashedKeyBytes, "AES")
+
+        // Create an instance of the cipher with the AES algorithm and CBC mode
+        val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
+
+        // Initialize the cipher in decryption mode with the key and initialization vector
+        cipher.init(Cipher.DECRYPT_MODE, key, IvParameterSpec(ivBytes))
+
+        // Decrypt the encrypted bytes
+        val decryptedBytes = cipher.doFinal(encryptedBytes)
+
+        // Convert the decrypted bytes back to a string and return it
+        Log.e("Logger Decode", String(decryptedBytes))
+
+    }
+
+    // Encrypt input to HashMD5
     private fun handleHashMD5(inputText: String): String {
         try {
             // Create MD5 Hash
@@ -49,7 +132,7 @@ class MainAct : AppCompatActivity() {
         return ""
     }
 
-    private fun compareMD5(inputText: String, hashMD5: String): Boolean{
+    private fun compareMD5(inputText: String, hashMD5: String): Boolean {
         val hashText = handleHashMD5(inputText)
         if (hashText == hashMD5) {
             return true
